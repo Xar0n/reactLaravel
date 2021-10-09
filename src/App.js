f52a73701a0bf3e68d17578019fdc1f75828edec
Xar0n <dima.dima.maksimov1998@mail.ru>
2021-10-09 12:17:40 +0800

    Login and Register pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import MasterLayout from "./layouts/admin/MasterLayout";
 import Home from "./components/frontend/Home";
+import Register from "./components/frontend/auth/Register";
+import Login from "./components/frontend/auth/Login";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Router>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/login/" component={Login} />
+                <Route exact path="/register/" component={Register} />
                 <Route path="/admin/" name="Admin" render={(props) => <MasterLayout {...props}/>}/>
             </Switch>
         </Router>
