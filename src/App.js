97e265bc12d18f1c58e17c7edcbb59d0181c887b
Xar0n <dima.dima.maksimov1998@mail.ru>
2021-10-11 23:24:15 +0800

    Logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import MasterLayout from "./layouts/admin/MasterLayout";
 import Home from "./components/frontend/Home";
 import Register from "./components/frontend/auth/Register";
@@ -11,14 +11,24 @@ axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.headers.post['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
+axios.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('auth_token');
+    config.headers.Authorization = token ? `Bearer ${token}`: '';
+    return config;
+});
+
 function App() {
   return (
     <div className="App">
         <Router>
             <Switch>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/login/" component={Login} />
-                <Route exact path="/register/" component={Register} />
+                <Route path="/login">
+                    {localStorage.getItem('auth_token') ? <Redirect to='/' />:<Login />}
+                </Route>
+                <Route path="/register">
+                    {localStorage.getItem('auth_token') ? <Redirect to='/' />:<Register />}
+                </Route>
                 <Route path="/admin/" name="Admin" render={(props) => <MasterLayout {...props}/>}/>
             </Switch>
         </Router>
--- src/layouts/frontend/Navbar.js
+++ src/layouts/frontend/Navbar.js
@@ -1,7 +1,44 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
+import swal from "sweetalert";
+import {useHistory} from "react-router-dom";
 
 function Navbar() {
+    var AuthButtons = '';
+    const  history = useHistory()
+    const logoutSubmit = (e) => {
+        e.preventDefault();
+        axios.post('/api/logout').then(res => {
+            if(res.data.status === 200) {
+                localStorage.removeItem('auth_token');
+                localStorage.removeItem('auth_name');
+                swal("Success", res.data.message, "success");
+                history.push('/');
+            }  else {
+
+            }
+        });
+    }
+
+    if(!localStorage.getItem('auth_token')) {
+        AuthButtons = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/register">Register</Link>
+                </li>
+            </ul>
+        );
+    } else {
+        AuthButtons = (
+            <li className="nav-item">
+                <button type="button" onClick={logoutSubmit} className="nav-link btn btn-danger btn-sm text-white">Logout</button>
+            </li>
+        );
+    }
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="#">Navbar</Link>
@@ -14,17 +51,12 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <Link className="nav-link" to="#">Home <span className="sr-only">(current)</span></Link>
+                        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="#">Collections</Link>
                     </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/login">Login</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/register">Register</Link>
-                    </li>
+                    {AuthButtons}
                 </ul>
             </div>
         </nav>
